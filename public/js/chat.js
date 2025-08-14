b009ab6fadb05e9de414700c02e92cefaf480dcd
Mamba09-bl <111229485+Mamba09-bl@users.noreply.github.com>
2025-08-14 22:25:30 +0500

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,7 @@
-
-
-  
-console.log("hello");
-
 const socket = io()
+console.log("chat.js loaded");
+console.log("chat.js updated", Date.now());
+
 
 let nameservice = prompt("Enter your name:")
 
@@ -12,7 +10,7 @@ document.querySelector("button").onclick = () =>{
     socket.emit("chat message",{
         text:message,
         username : nameservice,
-        
+         
     })
     document.querySelector("input").value = ""
 }
@@ -20,3 +18,18 @@ socket.on('chat show',(data)=>{
 const chatBox = document.querySelector("#chat");
 chatBox.innerHTML += `<p><strong>${data.username}:</strong> ${data.text}</p>`;
 })
+
+socket.on("chat history", (messages) => {
+    const chatBox = document.querySelector("#chat");
+    chatBox.innerHTML = ""; // 🔹 clear before adding
+    messages.forEach(msg => {
+        chatBox.innerHTML += `<p><strong>${msg.username}:</strong> ${msg.text}</p>`;
+    });
+    console.log(msg.username);
+    console.log(msg.text);
+
+    
+
+});
+
+
